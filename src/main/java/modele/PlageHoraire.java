94ec4eedfa6e09ac925d575a01d28592a984f959
Adam Planque <planque.adam@gmail.com>
2025-05-05 16:29:59 +0200

    Implement PlageHoraire and Reservation classes with duration validation and comparison logic

---
+++ src/main/java/modele/PlageHoraire.java
@@ -0,0 +1,37 @@
+package modele;
+public class PlageHoraire implements Comparable<PlageHoraire> {
+    private static final int DUREE_MIN = 15;
+    private Horaire debut;
+    private Horaire fin;
+
+    public PlageHoraire(Horaire debut, Horaire fin) {
+        this.debut = debut;
+        this.fin = fin;
+        int duree = getDuree();
+        if (duree < DUREE_MIN) {
+            throw new IllegalArgumentException("La durée doit être d'au moins " + DUREE_MIN + " minutes");
+        }
+    }
+
+    public int getDuree() {
+        int debutMinutes = debut.toMinutes();
+        int finMinutes = fin.toMinutes();
+        return finMinutes - debutMinutes;
+    }
+
+    public String toString() {
+        return debut + " - " + fin + " (" + getDuree() + " minutes)";
+    }
+
+    public int compareTo(PlageHoraire autre) {
+        int debutThis = this.debut.getHeure() * 60 + this.debut.getMinutes();
+        int debutAutre = autre.debut.getHeure() * 60 + autre.debut.getMinutes();
+        
+        if (debutThis < debutAutre) {
+            return -1;
+        } else if (debutThis > debutAutre) {
+            return 1;
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/modele/Reservation.java
@@ -0,0 +1,36 @@
+package modele;
+public class Reservation {
+    private PlageHoraire chPlageHoraire;
+    private String chTtitre;
+    private String chDate;
+
+    public Reservation(String parTitre, PlageHoraire parPlageHoraire, String parDate) {
+        this.chPlageHoraire = parPlageHoraire;
+        this.chTtitre = parTitre;
+        this.chDate = parDate;
+    }
+
+    public PlageHoraire getPlageHoraire() {
+        return chPlageHoraire;
+    }
+
+    public String getTitre() {
+        return chTtitre;
+    }
+
+    public String getDate() {
+        return chDate;
+    }
+
+    public String toString() {
+        return chTtitre + " le " + chDate + " de " + chPlageHoraire;
+    }
+
+    public int compareTo(Reservation autre) {
+        int compareDate = this.chDate.compareTo(autre.getDate());
+        if (compareDate != 0) {
+            return compareDate;
+        }
+        return chPlageHoraire.compareTo(autre.getPlageHoraire());
+    }
+}
